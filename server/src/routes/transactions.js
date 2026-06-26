e64f6146706091ccafd969e15e01b551514f1bcc
blissexodia <binnoley@gmail.com>
2026-06-26 14:29:42 +0545

    transactions changes

--- server/src/routes/transactions.js
+++ server/src/routes/transactions.js
@@ -5,16 +5,18 @@ import {
   deleteTransaction,
   getSummary,
   getMonthlyTrend,
+  getCategoryComparison,
 } from '../controllers/transactionController.js'
 import { protect } from '../middleware/auth.js'
 import { transactionRules, transactionQueryRules, validate } from '../middleware/validators.js'
 
 const router = express.Router()
 
-router.get('/summary',       protect, getSummary)
-router.get('/monthly-trend', protect, getMonthlyTrend)
-router.get('/',              protect, transactionQueryRules, validate, getTransactions)
-router.post('/',             protect, transactionRules, validate, createTransaction)
-router.delete('/:id',        protect, deleteTransaction)
+router.get('/summary',             protect, getSummary)
+router.get('/monthly-trend',       protect, getMonthlyTrend)
+router.get('/category-comparison', protect, getCategoryComparison)
+router.get('/',                    protect, transactionQueryRules, validate, getTransactions)
+router.post('/',                   protect, transactionRules, validate, createTransaction)
+router.delete('/:id',              protect, deleteTransaction)
 
 export default router
