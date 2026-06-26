cf944b93f96b03394bd6ea067ea0b25124c27ee4
blissexodia <binnoley@gmail.com>
2026-06-26 14:48:37 +0545

    RootProviders changes

--- client/components/providers/RootProviders.jsx
+++ client/components/providers/RootProviders.jsx
@@ -2,13 +2,16 @@
 
 import { ThemeProvider } from '@/context/ThemeContext'
 import { AuthProvider } from '@/context/AuthContext'
+import { LocaleProvider } from '@/context/LocaleContext'
 
 export default function RootProviders({ children }) {
   return (
     <ThemeProvider>
-      <AuthProvider>
-        {children}
-      </AuthProvider>
+      <LocaleProvider>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </LocaleProvider>
     </ThemeProvider>
   )
 }
